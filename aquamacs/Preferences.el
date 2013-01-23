896e2e4bce934823467360552c9d0381a2c99d99
Walter Moreira <walter@waltermoreira.net>
2013-01-23 14:19:56 -0600

    Add key for moving between buffers

--- aquamacs/Preferences.el
+++ aquamacs/Preferences.el
@@ -4,3 +4,9 @@
 ;; evaluating this file and print errors in the *Messags* buffer.
 ;; Use this file in place of ~/.emacs (which is loaded as well.)
 
+
+(global-set-key [M-left] 'windmove-left)          ; move to left windnow
+(global-set-key [M-right] 'windmove-right)        ; move to right window
+(global-set-key [M-up] 'windmove-up)              ; move to upper window
+(global-set-key [M-down] 'windmove-down)          ; move to downer window
+
