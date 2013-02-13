89de882787e5e12279248821bd474d34d68903a9
Walter Moreira <walter@waltermoreira.net>
2013-02-13 00:00:17 -0600

    A bit bigger font

--- aquamacs/customizations.el
+++ aquamacs/customizations.el
@@ -6,10 +6,11 @@
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 215 t)
  '(aquamacs-tool-bar-user-customization (quote ((37748974 Set\ for\ current\ session Save\ for\ future\ sessions Undo\ edits Reset\ to\ saved Erase\ customizations Help\ for\ Customize Exit))) t)
- '(default-frame-alist (quote ((cursor-type . box) (internal-border-width . 0) (modeline . t) (fringe) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (font . "-apple-Ubuntu_Mono-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns))))
+ '(default-frame-alist (quote ((cursor-type . box) (internal-border-width . 0) (modeline . t) (fringe) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (font . "-apple-Ubuntu_Mono-medium-normal-normal-*-15-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns))))
  '(frame-background-mode (quote dark))
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
+ '(one-buffer-one-frame-mode nil nil (aquamacs-frame-setup))
  '(python-shell-interpreter "ipython")
  '(rst-level-face-base-color "grey")
  '(rst-level-face-base-light 10)
@@ -22,5 +23,5 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Ubuntu_Mono"))))
- '(echo-area ((t (:stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Ubuntu_Mono")))))
+ '(default ((default nil) (nil (:height 150))))
+ '(echo-area ((default nil) (nil (:height 150)))))
