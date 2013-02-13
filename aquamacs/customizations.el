3e802af9e5784abca04226e6c3f58e0211b0b777
Walter Moreira <walter@waltermoreira.net>
2013-02-13 14:36:41 -0600

    Nicer modeline and desktop mode

--- aquamacs/customizations.el
+++ aquamacs/customizations.el
@@ -7,6 +7,7 @@
  '(aquamacs-customization-version-id 215 t)
  '(aquamacs-tool-bar-user-customization (quote ((37748974 Set\ for\ current\ session Save\ for\ future\ sessions Undo\ edits Reset\ to\ saved Erase\ customizations Help\ for\ Customize Exit))) t)
  '(default-frame-alist (quote ((cursor-type . box) (internal-border-width . 0) (modeline . t) (fringe) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (font . "-apple-Ubuntu_Mono-medium-normal-normal-*-15-*-*-*-m-0-iso10646-1") (fontsize . 0) (font-backend ns))))
+ '(desktop-save-mode t)
  '(frame-background-mode (quote dark))
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode (quote regular) t)
@@ -25,5 +26,8 @@
   ;; If there is more than one, they won't work right.
  '(default ((default nil) (nil (:height 150))))
  '(echo-area ((default nil) (nil (:height 150))))
- '(mode-line ((default nil) (nil (:height 140 :family "Ubuntu_Mono"))))
- '(mode-line-inactive ((default nil) (nil (:height 140 :family "Ubuntu_Mono")))))
+ '(mode-line ((t (:inherit aquamacs-variable-width :background "#073642" :foreground "#839496" :inverse-video t :box (:line-width 3 :color "#839496") :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Ubuntu_Mono"))))
+ '(mode-line-buffer-id ((((class color) (min-colors 89)) (:foreground "#839496" :weight bold))))
+ '(mode-line-flags ((default nil) (nil (:height 140 :family "Ubuntu_Mono"))))
+ '(mode-line-highlight ((t (:box (:line-width 2 :color "grey40" :style released-button)))))
+ '(mode-line-inactive ((t (:inherit aquamacs-variable-width :background "#073642" :foreground "#839496" :box (:line-width 3 :color "#073642") :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Ubuntu_Mono")))))
